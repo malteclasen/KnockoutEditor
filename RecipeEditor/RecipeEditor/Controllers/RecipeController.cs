1aa8e0e08aa412e28a841b88e2b0330dc564b866
clasen <malte.clasen@adesso.de>
2012-05-11 02:23:36 +0200

    prepared mvc4 application

---
+++ RecipeEditor/RecipeEditor/Controllers/RecipeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RecipeEditor.Models;
+
+namespace RecipeEditor.Controllers
+{
+    public class RecipeController : Controller
+    {
+        //
+        // GET: /Recipe/
+
+        public ActionResult Index()
+        {
+        	var model = new RecipeModel() {Title = "Streuselkuchen"};
+            return View(model);
+        }
+
+    }
+}
---
+++ RecipeEditor/RecipeEditor/Models/RecipeModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RecipeEditor.Models
+{
+	public class RecipeModel
+	{
+		public string Title { get; set; }
+	}
+}
